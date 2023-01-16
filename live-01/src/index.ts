e11f2b3f25413c1d2b4711af9f9d0f1e76f0bdfc
Nedymar <nedymar.schuabe@dataintegra.com.br>
2023-01-16 11:34:22 -0300

    Adicionado rota de usuarios (GET E POST)

--- live-01/src/index.ts
+++ live-01/src/index.ts
@@ -1,12 +1,20 @@
 
 import express, {Request, Response, NextFunction} from 'express';
+import userRoute from './routes/users.route';
 
 const app = express();
 
+// Configuração da Aplicação
+app.use(express.json());
+
+// Configuração das Rotas
+app.use(userRoute);
+
 app.get('/status', (req: Request, res: Response, next: NextFunction) => {
-    res.status(200).send({ foo: 'sucesso total!!!!'});
+    res.status(200).send({ foo: 'Sucesso!'});
 });
 
+//Inicialização do servidor
 app.listen(3000, () => {
     console.log('Aplicação executando na porta 3000!');
 });
---
+++ live-01/src/routes/users.route.ts
@@ -0,0 +1,29 @@
+/* get /users 
+get /users/:uuid 
+post /users 
+put /users/:randomUUID 
+delete /users/:uuid */
+
+import { Router, Response, Request, NextFunction } from 'express';
+import {StatusCodes} from 'http-status-codes';
+
+const userRoute = Router();
+
+userRoute.get('/users',(req: Request, res: Response, next: NextFunction) => {
+    const users = [{userName: 'Renan'}];
+    res.status(StatusCodes.OK).send({users});
+})
+
+userRoute.get('/users/:uuid',(req: Request<{uuid: string}>, res: Response, next: NextFunction) =>{
+    const uuid = req.params.uuid;
+    res.status(StatusCodes.OK).send({uuid});
+})
+
+userRoute.post('/users', (req: Request, res: Response, next: NextFunction) => {
+    const newUser = req.body;
+
+    console.log(req.body);
+    res.status(StatusCodes.CREATED).send(newUser);
+})
+
+export default userRoute;
