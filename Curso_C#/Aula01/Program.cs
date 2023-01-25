a8f6ed0adc9430d1f499ec15c285669f80dbe542
Nedymar <nedymar.schuabe@dataintegra.com.br>
2023-01-25 15:48:37 -0300

    Aula C#

---
+++ Curso_C#/Aula01/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+int valor1;
+int valor2;
+int total;
+
+Console.WriteLine("Digite o primeiro valor:");
+valor1 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Digite o Segundo valor:");
+valor2 = int.Parse(Console.ReadLine());
+
+total = valor1 + valor2;
+
+Console.WriteLine("Total: " + total);
---
+++ Curso_C#/Aula02/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+int idade;
+
+Console.WriteLine("Digite a idade:");
+idade = int.Parse(Console.ReadLine());
+
+if(idade >= 18) {
+    Console.WriteLine("É maior de Idade");
+} else {
+    Console.WriteLine("É menor de Idade");
+}
