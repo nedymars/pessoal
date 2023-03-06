dfac7acfa78162ff0978060b3bbdb07f9f05f5e1
Nedymar <80420358+nedymars@users.noreply.github.com>
2023-03-06 16:11:34 -0300

    PrimeiroProjeto_Ruby

---
+++ DIO/Ruby/aula3.rb
@@ -0,0 +1,8 @@
+print "Digite seu nome: "
+nome = gets.chomp
+print "Digite seu sobrenome: "
+sobrenome = gets.chomp
+print "Digite a sua idade: "
+idade = gets.chomp.to_i
+
+puts "#{nome} #{sobrenome} tem #{idade} anos!"
