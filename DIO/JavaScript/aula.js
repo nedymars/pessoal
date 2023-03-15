4704232c7a13485a89a386a4c36c94fb6fc9a19a
Nedymar <80420358+nedymars@users.noreply.github.com>
2023-03-15 11:28:16 -0300

    Aula1JS

---
+++ DIO/JavaScript/aula.js
@@ -0,0 +1,8 @@
+
+let precoCombustivel = 5.59;
+let qtdlitrosabastecimento = 45;
+let distanciaKM = 600;
+let valormediaConsumo = (distanciaKM / qtdlitrosabastecimento) * precoCombustivel;
+
+
+console.log(valormediaConsumo);
